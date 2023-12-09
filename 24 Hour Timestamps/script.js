7248cf62b355cb704e2b8cf6984c5d632ee589f2
Reapimus <29674188+Reapimus@users.noreply.github.com>
2023-12-09 23:23:02 +1100

    Create 24 hour timestamps

---
+++ 24 Hour Timestamps/script.js
@@ -0,0 +1,19 @@
+if (!window?._momentFormat) {
+    window._momentFormat = moment.prototype.format;
+}
+
+module.exports = {
+    load() {
+        console.log("24 hour timestamps loaded");
+        moment.prototype.format = function (str) {
+            str = str.replace("h", "H");
+            return window._momentFormat.bind(this)(str);
+        };
+    },
+    unload() {
+        console.log("24 hour timestamps unloaded");
+        moment.prototype.format = function (str) {
+            return window._momentFormat.bind(this)(str);
+        };
+    },
+}
